6f8b40ab503511c835a1ba13e6f889d3e2868d1c
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2021-04-17 00:14:59 +0530

    added analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,21 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head >
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-2GQ108D672"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window?.dataLayer.push(arguments)}
+                gtag('js', new Date());
+    
+                gtag('config', 'G-2GQ108D672');
+              `,
+            }}
+          />
+
+        </Head >
         <body className="overflow-auto">
           <Main />
           <NextScript />
